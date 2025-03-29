d71db064679e4bbab904d3c3a49578afdeaa9149
Evgenii Pechenkin <evgeniipechenkin@ukr.net>
2025-03-29 18:20:19 +0200

    Minor, non-essential code changes 2

--- app.js
+++ app.js
@@ -8,14 +8,6 @@ dotenv.config();
 
 const app = express();
 
-
-// Підключення до БД MySQL
-/*const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});*/
 //мідл
 app.use(cors());
 app.use(express.json());
